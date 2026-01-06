c484797aa23d66becada3e68a76ef31a5b268d8a
Austin Cronin <austin.cronin3d@gmail.com>
2026-01-05 21:38:03 -0600

    Hook up shows app

--- movietime/settings.py
+++ movietime/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shows.apps.ShowsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
--- movietime/urls.py
+++ movietime/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("shows/", include("shows.urls")),
+    path('', include('shows.urls')),
     path('admin/', admin.site.urls),
 ]
