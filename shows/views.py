dcd1ab4fb0c215e6bc6321d82c983d43e5d3a11f
Austin Cronin <austin.cronin3d@gmail.com>
2026-01-09 09:10:09 -0600

    Index template

--- shows/views.py
+++ shows/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, 'shows/index.html')
+
+    movies = Movie.objects.all()
+    context = {"movieList": movies}
+
+    return render(request, 'shows/index.html', context)
